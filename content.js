0eb5ceb38590865094cb2285f3883a6766ea121c
Shubham <shubhamvscode@gmail.com>
2023-03-16 16:57:21 +0530

    Fixing Undefined Bug

--- content.js
+++ content.js
@@ -1,22 +1,19 @@
 const playButton = document.getElementsByClassName("ytp-play-button")
 const forwardBotton = document.getElementsByClassName("ytp-forward-button")
 const backBotton = document.getElementsByClassName("ytp-replay-button")
-
 document.addEventListener("keydown", function (event) {
     if (event.code === "Space") {
         const videoScrollingDiv = document.getElementsByTagName("div")[4]
-        console.log("videoScrollingDiv", videoScrollingDiv)
-        videoScrollingDiv.style.overflowY = "hidden"
-
-        if (playButton[0].style.display == "none") {
-            playButton[1].click();
+        videoScrollingDiv?.style?.overflowY = "hidden"
+        if (playButton[0]?.style?.display == "none") {
+            playButton[1]?.click();
         } else {
-            playButton[0].click();
+            playButton[0]?.click();
         }
     }
     if (event.code === "ArrowLeft") {
-        backBotton[0].click();
+        backBotton[0]?.click();
     } else if (event.code === "ArrowRight") {
-        forwardBotton[0].click();
+        forwardBotton[0]?.click();
     }
 });
