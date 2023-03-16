d92695cc1a8f1bf9555676a3d228d2a8f10257e0
Shubham <shubhamvscode@gmail.com>
2023-03-16 15:49:39 +0530

    Left Right Space All Working

--- content.js
+++ content.js
@@ -1,10 +1,22 @@
 const playButton = document.getElementsByClassName("ytp-play-button")
+const forwardBotton = document.getElementsByClassName("ytp-forward-button")
+const backBotton = document.getElementsByClassName("ytp-replay-button")
+
 document.addEventListener("keydown", function (event) {
     if (event.code === "Space") {
+        const videoScrollingDiv = document.getElementsByTagName("div")[4]
+        console.log("videoScrollingDiv", videoScrollingDiv)
+        videoScrollingDiv.style.overflowY = "hidden"
+
         if (playButton[0].style.display == "none") {
             playButton[1].click();
         } else {
             playButton[0].click();
         }
+    }
+    if (event.code === "ArrowLeft") {
+        backBotton[0].click();
+    } else if (event.code === "ArrowRight") {
+        forwardBotton[0].click();
     }
 });
